ccacc441f5e1adfdc930d8647fe9d77eb0218288
Yannick Charron <ycharron@cstj.qc.ca>
2021-08-25 10:18:44 -0400

    Cours de vendredi

---
+++ S01/revision.js
@@ -0,0 +1,20 @@
+const theName = 'Yannick';
+let age = 33;
+
+console.log(theName);
+age++;
+console.log(age);
+
+const test = true + 1;
+const test2 = 125 + '9'; 
+console.log(test2);
+
+console.log(('b' + 'a' + + 'a' + 'a'));
+
+function displayUser(name, age) {
+    console.log(`Bonjour mon nom est ${name}, j'ai ${age} ans`);
+}
+
+displayUser('Rosalie',12);
+
+

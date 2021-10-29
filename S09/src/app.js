cb3c483028d38f9f80fc0bf0f8201ac90deb44d3
Yannick Charron <ycharron@cstj.qc.ca>
2021-10-29 12:45:26 -0400

    Début du cours - 29 octobre 2021

---
+++ S09/src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+import database from './libs/database.js';
+
+import errorMiddleware from './middlewares/errors.js';
+
+import planetsRoutes from './routes/planets.routes.js';
+import elementsRoutes from './routes/elements.routes.js';
+import explorationsRoutes from './routes/explorations.routes.js';
+
+database();
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/planets', planetsRoutes);
+app.use('/elements', elementsRoutes);
+app.use('/explorations', explorationsRoutes);
+
+app.use(errorMiddleware);
+
+export default app;
